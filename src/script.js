16f5d2d2e4814b7b396f53d669a00b80d2a62e39
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2023-03-15 16:26:28 +0000

    Implemented some of the functionality on the roll dice button on JS.

---
+++ src/script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// Selecting Elements
+const score0 = document.querySelector("#score0");
+const score1 = document.querySelector("#score1");
+const curScore0 = document.querySelector("#cur-score0");
+const curScore1 = document.querySelector("#cur-score1");
+
+const diceEl = document.querySelector("#dice");
+
+const newG = document.querySelector("#btn-new");
+const roll = document.querySelector("#btn-roll");
+const hold = document.querySelector("#btn-hold");
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Functions
+function newGame() {
+  alert("New Game");
+}
+
+function rollDice() {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  diceEl.classList.remove("hidden");
+  diceEl.src = `img/dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`cur-score${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`cur-score${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+}
+
+function holdBtn() {
+  alert("Hold");
+}
+
+//Starting Conditions
+score0.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add("hidden");
+
+//Event Listeners
+roll.addEventListener("click", rollDice);
+newG.addEventListener("click", newGame);
+hold.addEventListener("click", holdBtn);
